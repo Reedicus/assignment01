04010ea6c88f00c22ee4bf8885074789660ed9d6
student <student>
2017-03-09 18:17:50 -0600

    Final Develoment

---
+++ assignment02/EnactSprite.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+namespace assignment02
+{
+    public class EnactSprite : Sprite
+    {
+        int a;
+        private Bitmap image;
+        public Bitmap Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+
+        public override void act()
+        {
+            base.act();
+            if (a % 100 > 50)
+            {
+                this.X++;
+                this.Y++;
+            }
+
+            else
+            {
+                this.X--;
+                this.Y--;
+            }
+            a++;
+
+            //THIS IS WHERE YOU MAKE IT DO THINGS
+        }
+
+        public override void paint(Graphics g)
+        {
+            g.DrawImage(image, 0, 0);
+        }
+    }
+}
---
+++ assignment02/Form1.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Drawing;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace assignment02
+{
+    public partial class Form1 : Form
+    {
+        public static EnactSprite Sprite = new EnactSprite();
+        public static Form form;
+        public static Thread thread;
+        public static Graphics norm;
+        public static int fps = 30;
+        public static double running_fps = 30.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            form = this;
+            Sprite.Image = assignment02.Properties.Resources.BackGround;
+            thread = new Thread(new ThreadStart(Update));
+            thread.Start();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            thread.Abort();
+        }
+
+        public static void Update()
+        {
+            DateTime last = DateTime.Now;
+            DateTime now = last;
+            TimeSpan frameTime = new TimeSpan(10000000 / fps);
+            while (true)
+            {
+                DateTime temp = DateTime.Now;
+                running_fps = .9 * running_fps + .1 * 1000.0 / (temp - now).TotalMilliseconds;
+                Console.WriteLine(running_fps);
+                now = temp;
+                TimeSpan diff = now - last;
+                if (diff.TotalMilliseconds < frameTime.TotalMilliseconds)
+                    Thread.Sleep((frameTime - diff).Milliseconds);
+                last = DateTime.Now;
+                form.Invoke(new MethodInvoker(form.Refresh));
+                Sprite.act();
+            }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            Sprite.render(e.Graphics);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
+
